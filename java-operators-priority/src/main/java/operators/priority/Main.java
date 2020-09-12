6ec86ffcf536bb9deab136c6427d7ee192ff18cd
Eleonardo Oliveira <eleonardo.oliveira@anbetec.com.br>
2020-09-12 13:20:57 -0300

    Java Operators Priority!

---
+++ java-operators-priority/src/main/java/operators/priority/Main.java
@@ -0,0 +1,14 @@
+package operators.priority;
+
+public class Main {
+
+	public static void main(String[] args) {
+		boolean v = false;
+		
+		boolean x = 5 * 3 + Math.sqrt(7 * 7) <= 100 || !v && v;
+		
+		System.out.println("X Value: " + x);
+
+	}
+
+}
