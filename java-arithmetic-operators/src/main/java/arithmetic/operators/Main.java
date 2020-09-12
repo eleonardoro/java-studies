9f17743b2e341fa261958b875ae2082e3faf1564
Eleonardo Oliveira <eleonardo.oliveira@anbetec.com.br>
2020-09-12 12:57:27 -0300

    Java Arithmetic Operators!

---
+++ java-arithmetic-operators/src/main/java/arithmetic/operators/Main.java
@@ -0,0 +1,59 @@
+package arithmetic.operators;
+
+import br.com.softblue.commons.io.Console;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		String texto1;
+		String texto2;
+		
+		System.out.println("Type text 1: ");
+		text1 = Console.readString();
+		
+		System.out.println("Type text 2: ");
+		text2 = Console.readString();
+		
+		String bindedText = texto1 + texto2;
+		System.out.println("Binded Text: " + bindedText);
+		*/
+		
+		/*
+		System.out.println("Type the value of a:");
+		double a = Console.readInt();
+		
+		System.out.println("\nType the value of b:");
+		double b = Console.readInt();
+		
+		System.out.println("\nType the value of c:");
+		double c = Console.readInt();
+		
+		double delta = Math.pow(b, 2) - 4 * a * c;
+		
+		double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+		double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+		
+		System.out.println("\nX': " + x1);
+		System.out.println("X'': " + x2);
+		*/
+		
+		/*
+		System.out.println("Type the value of the side:");
+		int side = Console.readInt();
+		
+		int p = 6 * side;
+		double a = (3 * side * Math.sqrt(3)) / 2;
+		
+		System.out.println("\n\nPerimeter: " + p);
+		System.out.println("\nArea: " + a);
+		*/
+		
+		int div = 10 / 2;
+		int rest = 10 % 2;
+		
+		System.out.println("Div of 10 by 2: " + div);
+		System.out.println("\nRest of 10 by 2: " + rest);
+	}
+
+}
