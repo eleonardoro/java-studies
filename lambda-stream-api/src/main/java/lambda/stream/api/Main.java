b93c2c63f0212e98391d8a0a01a3cb82f9867c14
Eleonardo Oliveira <eleonardo.oliveira@anbetec.com.br>
2020-09-24 15:12:31 -0300

    Lambda and Stream Expressions API: Introduction!

---
+++ lambda-stream-api/src/main/java/lambda/stream/api/Main.java
@@ -0,0 +1,31 @@
+package lambda.stream.api;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		List<Integer> numbers = new ArrayList<>(List.of(5, 3, 4, 1, 2));
+		
+		System.out.println(numbers);
+		
+		//NumberComparator comparator = new NumberComparator();
+		
+//		Comparator<Integer> comparator = new Comparator<Integer>() {
+//			@Override
+//			public int compare(Integer o1, Integer o2) {
+//				return -o1.compareTo(o2);
+//			}
+//		};
+		
+		//Comparator<Integer> comparator = (o1, o2) -> -o1.compareTo(o2);
+		
+		numbers.sort((o1, o2) -> -o1.compareTo(o2));
+	
+		System.out.println(numbers);
+	}
+
+}
